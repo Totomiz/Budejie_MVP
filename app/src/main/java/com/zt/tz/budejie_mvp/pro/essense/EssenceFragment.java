67bce06aa268fcc3a62970b227e04284b9a6edc0
Totomiz <461609086@qq.com>
2017-10-16 21:52:49 +0800

    add:add:精华栏目子页Fragment

--- app/src/main/java/com/zt/tz/budejie_mvp/pro/essense/EssenceFragment.java
+++ app/src/main/java/com/zt/tz/budejie_mvp/pro/essense/EssenceFragment.java
@@ -1,17 +1,25 @@
 package com.zt.tz.budejie_mvp.pro.essense;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.zt.tz.budejie_mvp.R;
 import com.zt.tz.budejie_mvp.pro.base.view.BaseFragment;
+import com.zt.tz.budejie_mvp.pro.essense.view.adapter.EssenceAdapter;
 import com.zt.tz.budejie_mvp.pro.essense.view.navigation.EssenceNavigationBuilder;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by zhangtong on 2016-06-19 17:28
  * QQ:xxxxxxxx
  */
 public class EssenceFragment extends BaseFragment {
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
     @Override
     public int getContentView() {
         return R.layout.fragment_essence;
@@ -20,6 +28,8 @@ public class EssenceFragment extends BaseFragment {
     @Override
     public void initContentView(View viewContent) {
         initToolBar(viewContent);
+        mTabLayout = (TabLayout) viewContent.findViewById(R.id.tab_essence);
+        mViewPager= (ViewPager) viewContent.findViewById(R.id.vp_essence);
     }
 
     private void initToolBar(View viewContent) {
@@ -27,5 +37,15 @@ public class EssenceFragment extends BaseFragment {
         builder.createAndBind((ViewGroup) viewContent);
     }
 
+    @Override
+    public void initData() {
+        //获取StringArrs
+        String[] stringArray = getResources().getStringArray(R.array.essence_video_tab);
+        List<String> list= Arrays.asList(stringArray);
+        EssenceAdapter adapter=new EssenceAdapter(getFragmentManager(),list);
+
+        mViewPager.setAdapter(adapter);
+        mTabLayout.setupWithViewPager(mViewPager);
+    }
 
 }
