f4972d967712cea9dc9d32d7677da7a10afd9e28
Totomiz <461609086@qq.com>
2017-10-17 21:25:14 +0800

    add:add bean

---
+++ app/src/main/java/com/zt/tz/budejie_mvp/bean/IBean.java
@@ -0,0 +1,9 @@
+package com.zt.tz.budejie_mvp.bean;
+
+/**
+ * Created by tony.zhang
+ * Email: tony.zhang.Mr@foxmail.com
+ */
+
+public interface IBean {
+}
---
+++ app/src/main/java/com/zt/tz/budejie_mvp/bean/PositionBean.java
@@ -0,0 +1,12 @@
+package com.zt.tz.budejie_mvp.bean;
+
+import com.google.gson.Gson;
+
+/**
+ * Created by tony.zhang
+ * Email: tony.zhang.Mr@foxmail.com
+ */
+
+public class PositionBean extends Bean{
+    private static Gson gson=new Gson();
+}
